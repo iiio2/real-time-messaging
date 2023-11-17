57138731e55e218a22422f0e0ff9a12263794d9d
Mr. Robot <gomexxxx71@gmail.com>
2023-11-17 21:59:15 +0600

    chore: use daphne server

--- real_time_messaging/asgi.py
+++ real_time_messaging/asgi.py
@@ -11,6 +11,18 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time_messaging.settings')
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-application = get_asgi_application()
+import room.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            room.routing.websocket_urlpatterns
+        )
+    )
+})
