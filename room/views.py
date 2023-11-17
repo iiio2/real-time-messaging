7692560db97e7efe8823a2844d3e91936b8805d0
Mr. Robot <gomexxxx71@gmail.com>
2023-11-17 21:35:06 +0600

    chore: styling

--- room/views.py
+++ room/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from . models import Room
 
 # Create your views here.
 
 def rooms(request):
-  return render(request, 'room/rooms.html')
+  rooms = Room.objects.all()
+  return render(request, 'room/rooms.html', { 'rooms':rooms })
